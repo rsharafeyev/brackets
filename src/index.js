2eb5b225838e8da858dab0f9550d9f00745fdb01
rsharafeyev <r.sharafeyev@gmail.com>
2021-03-03 13:26:29 +0600

    init: created brackets check funcion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let repeat = true;
+  while(repeat)
+  {
+    repeat = false;
+    for(item of bracketsConfig)
+    {
+      let pair = item[0] + item[1];
+      while(str.includes(pair))
+      {
+        str = str.slice(0,str.indexOf(pair)) + str.slice(str.indexOf(pair) + pair.length);
+        repeat = true;
+      }
+    }
+  }
+  if(str){return false;} else {return true;}
 }
